e5b812a6d83a52a8f27e4ca15c50acdf8e40a4c6
Daniel Podrabinek <dpodrabinek@oktopost.com>
2016-10-23 15:47:34 +0300

    Added dummyController example

---
+++ controller/DummyControllerExample.php
@@ -0,0 +1,56 @@
+<?php
+
+
+use Tattler\Common;
+use Tattler\Base\Channels\IRoom;
+use Tattler\Base\Channels\IUser;
+use Tattler\Base\Modules\ITattler;
+
+
+/**
+ * Class DummyControllerExample
+ */
+class DummyControllerExample
+{
+    /** @var ITattler $tattler */
+    private $tattler;
+
+
+    /**
+     */
+    public function __construct()
+    {
+        $this->tattler = Common::skeleton(ITattler::class);
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getWs()
+    {
+        return ['ws' => $this->tattler->getWsAddress()];
+    }
+
+
+    /**
+     * @param string     $socketId
+     * @param null|array $channels
+     * @return array
+     */
+    public function getChannels($socketId, $channels = null)
+    {
+        /** @var IUser $user */
+        $user = Common::skeleton(IUser::class);
+        $user->setName('current', 'user', 'name', 'with', 'any', 'args')
+            ->setSocketId($socketId);
+
+        $this->tattler->setUser($user);
+
+        /** @var IRoom $newRoom */
+        $newRoom = Common::skeleton(IRoom::class);
+        $newRoom->setName('newRoom')->allow($user);
+
+        return ['channels' => $this->tattler->getChannels($channels)];
+    }
+}
