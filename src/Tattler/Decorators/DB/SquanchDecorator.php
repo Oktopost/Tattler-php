bb7cc87c74ae273dc1c1c20d58877e3584f8ad82
Daniel Podrabinek <dpodrabinek@oktopost.com>
2017-04-15 22:00:50 +0300

    Fixed DB\SquanchDecorator

--- src/Tattler/Decorators/DB/SquanchDecorator.php
+++ src/Tattler/Decorators/DB/SquanchDecorator.php
@@ -2,8 +2,9 @@
 namespace Tattler\Decorators\DB;
 
 
-use Objection\LiteObject;
 use Objection\Mapper;
+use Objection\LiteObject;
+
 use ReflectionClass;
 use Squanch\Base\ICachePlugin;
 
@@ -16,8 +17,6 @@ class SquanchDecorator implements IDBDecorator
 	/** @var ICachePlugin */
 	private $client;
 	
-	private $bucket;
-	
 	
 	/**
 	 * @param LiteObject $object
@@ -29,20 +28,11 @@ class SquanchDecorator implements IDBDecorator
 		return $reflection->getShortName();
 	}
 	
-	
-	/**
-	 * @return ICachePlugin
-	 */
-	public function getConnection()
-	{
-		return $this->client;
-	}
-	
 	/**
 	 * @param TattlerAccess $object
 	 * @return string
 	 */
-	private function getAccessObjectKey(TattlerAccess $object)
+	private function getBucketName(TattlerAccess $object)
 	{
 		return $this->getClassShortName($object).':'.$object->UserToken;
 	}
@@ -52,19 +42,27 @@ class SquanchDecorator implements IDBDecorator
 	 * @param ICachePlugin $squanch
 	 * @param string $bucket
 	 */
-	public function __construct(ICachePlugin $squanch, $bucket = 'tattler')
+	public function __construct(ICachePlugin $squanch)
 	{
 		$this->client = $squanch;
-		$this->bucket = $bucket;
+	}
+	
+	
+	/**
+	 * @return ICachePlugin
+	 */
+	public function getConnection()
+	{
+		return $this->client;
 	}
 	
 	
 	public function insertAccess(TattlerAccess $access, $ttl)
 	{
 		return $this->client->set()
-			->setKey($this->getAccessObjectKey($access))
+			->setKey($access->Channel)
 			->setData(Mapper::getJsonFor($access))
-			->setBucket($this->bucket)
+			->setBucket($this->getBucketName($access))
 			->setTTL($ttl)
 			->execute();
 	}
@@ -76,10 +74,12 @@ class SquanchDecorator implements IDBDecorator
 	 */
 	public function updateAccessTTL(TattlerAccess $access, $newTTL)
 	{
-		return $this->client->has()
-			->byKey($this->getAccessObjectKey($access))
-			->byBucket($this->bucket)
-			->resetTTL($newTTL)
+		return $this->client->set()
+			->updateOnly()
+			->setKey($access->Channel)
+			->setBucket($this->getBucketName($access))
+			->setTTL($newTTL)
+			->setData($access)
 			->execute();
 	}
 	
@@ -90,8 +90,8 @@ class SquanchDecorator implements IDBDecorator
 	public function accessExists(TattlerAccess $access)
 	{
 		return $this->client->has()
-			->byKey($this->getAccessObjectKey($access))
-			->byBucket($this->bucket)
+			->byKey($access->Channel)
+			->byBucket($this->getBucketName($access))
 			->execute();
 	}
 	
@@ -102,8 +102,8 @@ class SquanchDecorator implements IDBDecorator
 	public function deleteAccess(TattlerAccess $access)
 	{
 		return $this->client->delete()
-			->byKey($this->getAccessObjectKey($access))
-			->byBucket($this->bucket)
+			->byKey($access->Channel)
+			->byBucket($this->getBucketName($access))
 			->execute();
 	}
 	
@@ -116,12 +116,13 @@ class SquanchDecorator implements IDBDecorator
 	{
 		$tmpAccess = new TattlerAccess();
 		$tmpAccess->UserToken = $userToken;
-		$data = $this->client->get()->byBucket($this->getAccessObjectKey($tmpAccess))->asCollection();
+		$data = $this->client->get()->byBucket($this->getBucketName($tmpAccess))->asCollection();
 		
 		if (!$data)
 			return false;
 		
 		$result = $data->asLiteObjects(TattlerAccess::class);
+		
 		$locked = [];
 		
 		/** @var TattlerAccess $value */
@@ -153,9 +154,9 @@ class SquanchDecorator implements IDBDecorator
 		
 		return $this->client->set()
 			->updateOnly()
-			->setKey($this->getAccessObjectKey($access))
+			->setKey($access->Channel)
 			->setData(Mapper::getJsonFor($access))
-			->setBucket($this->bucket)
+			->setBucket($this->getBucketName($access))
 			->execute();
 	}
 	
@@ -169,9 +170,9 @@ class SquanchDecorator implements IDBDecorator
 		
 		return $this->client->set()
 			->updateOnly()
-			->setKey($this->getAccessObjectKey($access))
+			->setKey($access->Channel)
 			->setData(Mapper::getJsonFor($access))
-			->setBucket($this->bucket)
+			->setBucket($this->getBucketName($access))
 			->execute();
 	}
 	
