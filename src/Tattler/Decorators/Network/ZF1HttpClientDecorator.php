5512996291c14c7ca466a025e13b320b460bb8dc
Daniel Podrabinek <dpodrabinek@oktopost.com>
2017-04-15 22:41:30 +0300

    Added ZF1HttpClientDecorator

---
+++ src/Tattler/Decorators/Network/ZF1HttpClientDecorator.php
@@ -0,0 +1,76 @@
+<?php
+namespace Tattler\Decorators\Network;
+
+
+use \Zend_Http_Client;
+use Tattler\Base\Decorators\INetworkDecorator;
+
+
+class ZF1HttpClientDecorator implements INetworkDecorator
+{
+	/** @var Zend_Http_Client */
+	private $client;
+	
+	
+	private function getHeaders()
+	{
+		return [
+			'Content-Type: application/json'
+		];
+	}
+	
+	
+	public function __construct()
+	{
+		$this->client = new Zend_Http_Client();
+	}
+	
+	/**
+	 * @param array $tattlerBag
+	 * @return bool
+	 */
+	public function sendPayload(array $tattlerBag)
+	{
+		$this->client
+			->setUri($tattlerBag['tattlerUri'])
+			->setMethod(\Zend_Http_Client::POST)
+			->setHeaders($this->getHeaders())
+			->setRawData(json_encode($tattlerBag['payload']))
+			->request();
+		
+		if ($this->client->getLastResponse()->getStatus() == 200)
+		{
+			$body = json_decode($this->client->getLastResponse()->getBody(), true);
+			
+			if (isset($body['status']) && $body['status'] == 200)
+			{
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * @param array $tattlerBag
+	 * @return array|bool
+	 */
+	public function syncChannels(array $tattlerBag)
+	{
+		$this->client
+			->setUri($tattlerBag['tattlerUri'])
+			->setMethod(\Zend_Http_Client::POST)
+			->setHeaders($this->getHeaders())
+			->setRawData(json_encode($tattlerBag['payload']))
+			->request();
+		
+		
+		if ($this->client->getLastResponse()->getStatus() == 200)
+		{
+			$body = json_decode($this->client->getLastResponse()->getBody(), true);
+			return isset($body['rooms']) ? $body['rooms'] : false;
+		}
+		
+		return false;
+	}
+}
